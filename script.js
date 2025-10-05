dbf28f093afb6f33be7302d00324c99666087be1
Kowshika.S <140044473+kowshika555@users.noreply.github.com>
2025-10-05 23:31:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-// Smooth Scroll for navbar links
+// Smooth Scroll
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
     document.querySelector(link.getAttribute('href'))
       .scrollIntoView({ behavior: 'smooth' });
   });
+});
+
+// Mobile Menu Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
 });
